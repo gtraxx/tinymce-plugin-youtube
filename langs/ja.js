493b127f5109ee4cfa83d329a7fcd77502e9552c
Hyodo Tatsuya <t_hyodo@enfactory.co.jp>
2018-03-01 15:30:04 +0900

    add Japanese lang

---
+++ langs/ja.js
@@ -0,0 +1,13 @@
+tinymce.addI18n('ja', {
+    'YouTube Tooltip'   : "YouTube",
+    'YouTube Title'     : "Youtube埋め込み",
+    'Youtube URL'       : 'URL',
+    'Youtube ID'        : 'http://youtu.be/xxxxxxxx または http://www.youtube.com/watch?v=xxxxxxxx',
+    'width'             : '幅',
+    'height'            : '高さ',
+    'autoplay'          : '自動再生',
+    'Related video'     : '関連動画を表示',
+    'HD video'          : 'HD再生を行う',
+    'cancel'            : 'キャンセル',
+    'Insert'            : '挿入する'
+});
