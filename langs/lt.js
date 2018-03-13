8d5131d7162c26f77bcc095fc333ae2506085dc2
gtraxx <gerits.aurelien@gmail.com>
2018-03-13 09:03:10 +0100

    Add Lithuanian translate and fix youtube.html

---
+++ langs/lt.js
@@ -0,0 +1,13 @@
+tinymce.addI18n('lt', {
+    'YouTube Tooltip' : "YouTube",
+    'YouTube Title' : "Įterpti YouTube Video",
+    'Youtube URL' : 'Nuoroda',
+    'Youtube ID' : 'http://youtu.be/xxxxxxxx arba http://www.youtube.com/watch?v=xxxxxxxx',
+    'width' : 'Plotis',
+    'height' : 'Aukštis',
+    'autoplay' : 'Autoplay',
+    'Related video' : 'Susijęs video',
+    'HD video' : 'HD kokybė',
+    'cancel' : 'Atšaukti',
+    'Insert' : 'Įterpti'
+});
